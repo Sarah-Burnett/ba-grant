eb6edff051e605b787ea6031cc51020000b0d9d4
Sarah-Burnett <burnie8657@gmail.com>
2020-06-05 17:31:38 +0100

    manual build

---
+++ handover/build/precache-manifest.d08f2c0989f20c37ee1015cba52ab9d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69742ac5ebf010e7bb73caf485c386d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20599d5c579963fd4381",
+    "url": "/static/css/main.9fc0cb99.chunk.css"
+  },
+  {
+    "revision": "4bfb8d240cb9c6595a7e",
+    "url": "/static/js/2.144e1834.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.144e1834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20599d5c579963fd4381",
+    "url": "/static/js/main.0f93abd8.chunk.js"
+  },
+  {
+    "revision": "624a466c21b97001d425",
+    "url": "/static/js/runtime-main.dcd4706d.js"
+  },
+  {
+    "revision": "2509b3053cf22225c3efa87e22664a1e",
+    "url": "/static/media/E-Prime2-logo.2509b305.jpg"
+  },
+  {
+    "revision": "15caf509a1de1d5ea0243339ee615b2a",
+    "url": "/static/media/biopac-logo.15caf509.png"
+  },
+  {
+    "revision": "98fe617fb9a459b98d5021f4e9e3cc2f",
+    "url": "/static/media/qualtrics-logo.98fe617f.jpg"
+  },
+  {
+    "revision": "15b4c88e58af43b054d6c73f5e6e9d4f",
+    "url": "/static/media/sharepoint-logo.15b4c88e.png"
+  },
+  {
+    "revision": "751952a4fd408a492a25c3ecf87499da",
+    "url": "/static/media/sona-logo.751952a4.PNG"
+  }
+]);
